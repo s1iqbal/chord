feb972afa2f1e580cf864a63f78e230a6046120b
Scheisse <jjwazoo@gmail.com>
2021-03-02 17:26:52 -0500

    Global rate limiting

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ const client = new Banchojs.BanchoClient({
 
 const prefix = "!";
 
+//record of the execution times of the most recent n executions
+const MESSAGE_CAP = 10; //max number of messages that can be sent within the window
+const WINDOW = 5; //time frame for rate limiting (in seconds)
+var lastEx = []; //want to store last MESSAGE_CAP+1 execs
+
 const startOsuBot = async () => {
     //connect to bancho
     try {
@@ -40,17 +45,20 @@ const startOsuBot = async () => {
                 const filters = message.split(" ");
                 switch(command) {
                     case prefix + "hello":
-                        return await user.sendMessage(`Hello, ${user.ircUsername}`);
+                        if(!canExecute(1)) return;
+                        return await user.sendMessage(`Hello, ${user.ircUsername}`).then(logExec());
                     case prefix + "help":
                     case prefix + "commands":
-                        await user.sendMessage(`!r (MMR) stars=(1-10) bpm=(50-300) mod=(hardrock, hidden, doubletime, freemod, nomod, tiebreaker)`)
-                        return await user.sendMessage(`THE MMR IS REQUIRED RIGHT AFTER !r Example: "!r 1500 mod=hardrock stars=4.52 bpm=93`);;
+                        if (!canExecute(2)) return;
+                        await user.sendMessage(`!r (MMR) stars=(1-10) bpm=(50-300) mod=(hardrock, hidden, doubletime, freemod, nomod, tiebreaker)`).then(logExec())
+                        return await user.sendMessage(`THE MMR IS REQUIRED RIGHT AFTER !r Example: "!r 1500 mod=hardrock stars=4.52 bpm=93`).then(logExec());;
                     case prefix + "request" :
                     case prefix + "r":
                         //send the command to a filter to process
                         if (filters.length == 2 && typeof(+filters[1])=="number") {
                             if (+filters[1] < 1150 || +filters[1] > 3300) {
-                                await user.sendMessage('Please enter an MMR between 1150-3300');
+                                if (!canExecute(1)) return;
+                                await user.sendMessage('Please enter an MMR between 1150-3300').then(logExec());
                                 return;
                             }
                             message = findMapFilteredMMR(
@@ -58,7 +66,9 @@ const startOsuBot = async () => {
                                     findPools(+filters[1], 50, mapPools)
                                 )
                             );
-                            await Promise.all([user.sendMessage(message[0]), user.sendMessage(message[1])]);
+                            //Record the execution time after each message is sent
+                            if (!canExecute(2)) return;
+                            await Promise.all([user.sendMessage(message[0]), logExec(), user.sendMessage(message[1]), logExec()]);
                             return;
                         } else if (filters.length > 2) {
                             //find mmr
@@ -70,10 +80,11 @@ const startOsuBot = async () => {
                                 )
    
                                 if (typeof m === 'string') {
-                                    await user.sendMessage(m);
+                                    await user.sendMessage(m).then(logExec());
                                 } else {
                                     // await user.sendMessage(`MMR: ${+filters[1]-50}-${+filters[1]+50}`);
-                                    await Promise.all([user.sendMessage(m[0]), user.sendMessage(m[1])]);
+                                    if (!canExecute(2)) return;
+                                    await Promise.all([user.sendMessage(m[0]), logExec(), user.sendMessage(m[1]), logExec()]);
                                 }
                                 return;
                             }
@@ -86,7 +97,8 @@ const startOsuBot = async () => {
                                 )
                             );
                             let m0 = `Random MMR Selected - MMR: ${mmr-range} - ${mmr+range} - Please use !r 1150 to request a map with 1150 mmr. Use !commands to view more`;
-                            await Promise.all([user.sendMessage(m0), user.sendMessage(message[0])], user.sendMessage(message[1]));
+                            if (!canExecute(3)) return;
+                            await Promise.all([user.sendMessage(m0), logExec(), user.sendMessage(message[0])], logExec(), user.sendMessage(message[1]), logExec());
                             return;
                         }
                 }
@@ -97,4 +109,29 @@ const startOsuBot = async () => {
     } catch(err) {
         console.error(err);
     }
+}
+
+//determine if [needed] amount of messages can be appended to logExec without violating window length limit
+let canExecute = (needed) =>{
+    //if lastEx has not grown enough yet, can always execute
+    if(needed>lastEx.length) return true;
+
+    var time = Date.now()/1000; //curr time in seconds
+    //lastEx[needed-1] will become the starting time of the window after adding new executions
+    var span = time - lastEx[needed-1];
+    
+    //log when window exceeded
+    if(span<WINDOW) console.log("Warning: Message rate limit exceeded!");
+
+    return span>WINDOW; //can execute if the time span is greater than the minimum accepted WINDOW
+}
+
+//FIXME: could be moved to helpers.js? idk
+let logExec = () => {
+    //get time in seconds
+    var time = Date.now()/1000;
+
+    //if lastEx list already at cap of executions
+    if(lastEx.length>=MESSAGE_CAP+1) lastEx=lastEx.slice(-1*(MESSAGE_CAP)); //narrow down list to only the most recent n executions
+    lastEx.push(time); //push new exec time   
 }
