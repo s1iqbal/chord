6d13983c03b59e3ca7193c979ed6713c3abcfbad
Saad Iqbal <snipingattracts@gmail.com>
2026-02-18 18:28:28 -0500

    add api url to env for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/recommend': 'http://localhost:5000',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/recommend': env.VITE_BASE_API_URL,
+      },
     },
-  },
+  }
 })
